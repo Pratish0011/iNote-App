eea614a32bf25585a5defb43a110e2c87a3f0554
Pratish Bhandari <pratishbhandari2@gmail.com>
2023-12-19 23:04:03 +0545

    "Build Changes"

--- api/index.js
+++ api/index.js
@@ -37,6 +37,12 @@ app.use('/api/notes', noteRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 
 app.use((err,req,res, next)=>{
   const statusCode = err.statusCode || 500
